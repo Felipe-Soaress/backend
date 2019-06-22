42ca6ec36e248d287d4f1af7284667251d409a49
Felipe Soares <felipe_soares_2@hotmail.com>
2019-06-22 00:16:32 -0300

    Ajustes Login

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,4 +1,7 @@
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+
+
 
 class LoginController {
     // async store(req, res) {
@@ -7,10 +10,13 @@ class LoginController {
     // }
 
     async show(req, res) {
-        console.log("log:",req.query);
+        const consulta = await User.find({username:req.query.username});
+        var email = CryptoJS.SHA256(consulta[0].email).toString(CryptoJS.enc.Base64);
+        var emailPass = email + req.query.password;
+        var Pass = CryptoJS.SHA256(emailPass).toString(CryptoJS.enc.Base64);
+
         const login = await User.findOne({ username: req.query.username,
-                                            password: req.query.password });
-        console.log(login)
+                                            password: Pass });
         return res.json(login);
     }
 }
