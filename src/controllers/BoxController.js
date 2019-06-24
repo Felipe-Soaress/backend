a708b7d74c9c9efc9c1830567da392a3f2dcc300
Leonardo <leonardo_tsuji@hotmail.com>
2019-06-24 00:39:42 -0300

    trying to encrypt files

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -1,5 +1,13 @@
 const Box = require('../models/Box');
 
+const publicKey =
+    `-----BEGIN PUBLIC KEY-----
+        MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCvslv8UjnU9T3RG6WwJOTJ0EdS
+        AFxMIT6N/eJ704Mh0CkksAD2hdspEJ5Qq07b6DPQcpI5zK1DQqC50vZMnP/hTZlU
+        GbNxoF5JXgZn13ziUq9eL1ACayLBOiywmogG/Icg78vOqmDvaURClMXjARsjHX4X
+        9rWUTwhBguBzL12BMQIDAQAB
+        -----END PUBLIC KEY-----`;
+
 class BoxController{
    async store(req,res) {
     
